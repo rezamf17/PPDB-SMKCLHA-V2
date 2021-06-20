93b1e12abc1efbfd92da72b01f18ec0292cdd7df
rezamf17 <rezayuyara@gmail.com>
2021-06-20 21:19:58 +0700

    menambahkan cara daftar

--- resources/views/caraDaftar.blade.php
+++ resources/views/caraDaftar.blade.php
@@ -3,6 +3,22 @@
 Cara Daftar
 @endsection
 @section('content')
-ASD
-<img src="..." class="img-fluid" alt="...">
+<h1 class="judulCara">Cara Daftar Lewat Laptop/PC</h1>
+<p>1. Klik 'DAFTAR SEKARANG'</p>
+<img src="{{asset('gambar/screenShot/homeArrow.png')}}" class="gambar1">
+<p>2. Silahkan isi form yang telah disediakan. Lalu, klik 'DAFTAR'</p>
+<img src="{{asset('gambar/screenShot/daftarsekarangArrow.png')}}" class="gambar1">
+<p>3. Setelah berhasil akan muncul tampilan menu utama. Pilih Menu 'Formulir' untuk mengisi Formulir</p>
+<img src="{{asset('gambar/screenShot/dashboardSiswaArrow.png')}}" class="gambar1">
+<p>4. Setelah klik menu Formulir, isikan dengan lengkap Formulir data diri Anda.</p>
+<img src="{{asset('gambar/screenShot/formulirArrow.png')}}" class="gambar1">
+<p>5. Setelah mengisi Formulir dengan lengkap. Klik Simpan.</p>
+<img src="{{asset('gambar/screenShot/simpanformulir.png')}}" class="gambarSimpan">
+<p>6. Setelah itu akan kembali ke menu utama. Klik Menu 'Cetak Pendaftaran'</p>
+<img src="{{asset('gambar/screenShot/dashboardSiswaArrow2.png')}}" class="gambar1">
+<p>7. Berikut, adalah contoh hasil formulir pendaftaran yang sudah jadi dalam bentuk file PDF. Print file tersebut untuk mendaftar ulang ke sekolah.</p>
+<img src="{{asset('gambar/screenShot/report.png')}}" class="gambar1">
+<p>8. Setelah di Print silahkan datang langsung ke SMK Cellular Hauro'49 untuk mendaftar ulang dan jangan lupa untuk membawa dokumen yang diperlukan.</p>
+
+
 @endsection
