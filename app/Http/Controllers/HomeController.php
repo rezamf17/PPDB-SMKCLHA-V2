671e9015fa7d646efec70f513c2647b0a0502e02
Reza M Fauzi | 17111117 <58104687+rezamf17@users.noreply.github.com>
2021-08-13 20:31:55 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
-use App\form;
+use App\Form;
 use Auth;
 
 class HomeController extends Controller
